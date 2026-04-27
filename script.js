54d34f51add0253b738995bbba29128c0d254129
GuiThuler <guilhermevthuler@gmail.com>
2026-04-27 13:49:19 -0300

    Função para adicionar lembrete separada. Função de ganhos e gasto adicionada.

--- script.js
+++ script.js
@@ -1,20 +1,74 @@
-// ADD A CATEGORIA E VALOR
+function adicionarEvent() {
 
-function adicionarLembreteEvent() {
+  const formLembrete = document.querySelector('.formLembrete');
+  const formGanho = document.querySelector('.formGanho');
+  const formGasto = document.querySelector('.formGasto');
 
-  const form = document.querySelector('.form');
-  const resultado = document.querySelector('.resultado');
-  const lembreteAdicionado = [];
+  const resultadoLembrete = document.querySelector('.resultadoLembrete');
+  const resultadoGanho = document.querySelector('.resultadoGanho');
+  const resultadoGasto = document.querySelector('.resultadoGasto');
 
-  function FormEvent(evento) {
+  function formEvent(evento) {
     evento.preventDefault();
 
-    const addLembrete = form.querySelector('.addLembrete');
-    lembreteAdicionado.push(addLembrete.value);
-    resultado.innerHTML += `<p>${addLembrete.value}</p>`;
+    setTimeout(() => {
+            this.reset(); // Limpa todos os campos do formulário , o 1 siginifica milisegundo
+        }, 1);
 
-    console.log(lembreteAdicionado);
+    // ADD LEMBRETES
+    
+     function lembreteEvent() {
+
+      const lembrete = [];
+      const addLembrete = formLembrete.querySelector('.addLembrete');
+
+      lembrete.push(addLembrete.value);
+      resultadoLembrete.innerHTML += `<p>${addLembrete.value}</p>`;
+
+    }
+
+    lembreteEvent()
+
+    // ADD CATEGORIA E VALOR DOS GASTOS E GANHOS
+
+    function eventGanho() {
+      const valorCategoriaGanho = [{categoriaGanho: '' , valorGanho: ''}];
+
+      const [{categoriaGanho , valorGanho}] = valorCategoriaGanho;
+      const addCategoriaGanho = formGanho.querySelector('.addCategoriaGanho');
+      const addValorGanho = formGanho.querySelector('.addValorGanho');
+
+      console.log(categoriaGanho , valorGanho)
+
+      valorCategoriaGanho.push(addCategoriaGanho.value , Number(addValorGanho.value));
+
+      resultadoGanho.innerHTML += `<p>${addCategoriaGanho.value}  ${addValorGanho.value}</p>`;
+    }
+
+    eventGanho()
+
+    function eventGasto() {
+      const valorCategoriaGasto = [{categoriaGasto: '' , valorGasto: ''}];
+
+      const [{categoriaGasto , valorGasto}] = valorCategoriaGasto;
+      const addCategoriaGasto = formGasto.querySelector('.addCategoriaGasto');
+      const addValorGasto = formGasto.querySelector('.addValorGasto');
+
+      console.log(categoriaGasto , valorGasto)
+
+      valorCategoriaGasto.push(addCategoriaGasto.value , Number(addValorGasto.value));
+
+      resultadoGasto.innerHTML += `<p>${addCategoriaGasto.value}  ${addValorGasto.value}</p>`;
+    }
+
+    eventGasto()
+
   }
-  form.addEventListener("submit", FormEvent);
+  formLembrete.addEventListener("submit", formEvent);
+  formGanho.addEventListener("submit", formEvent);
+  formGasto.addEventListener("submit", formEvent);
+
 }
-adicionarLembreteEvent();
+adicionarEvent();
+
+
